3bd6884bee83b7ff150f1f5ad3cf336b36d0387f
yVentura7 <168231053+yVentura7@users.noreply.github.com>
2024-10-21 18:37:46 -0500

    1

---
+++ app/src/main/java/com/example/lab12/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.lab12
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab12.ui.theme.Lab12Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MapScreen()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab12Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -0,0 +1,37 @@
+package com.example.lab12
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Marker
+
+
+@Composable
+fun MapScreen() {
+    val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
+    val cameraPositionState = rememberCameraPositionState {
+        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
+    }
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Añadir GoogleMap al layout
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            // Añadir marcador en Denver, Colorado
+            Marker(
+                state = rememberMarkerState(position = ArequipaLocation),
+                title = "Arequipa, Perú"
+            )
+        }
+    }
+}
